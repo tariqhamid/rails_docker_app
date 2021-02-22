3a9ad4d981a677d7ac2b3192045dd76a69d309d2
Tariq Hamid <tariq@hamid.com>
2021-02-22 18:42:27 +0000

    Add initial scripts, and change to the HTML part

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,12 @@
 class HomeController < ApplicationController
     def index
       @message = "Dynamic"
-
+  
       @posts = Post.all
+    end
+  
+    def increment_async
+      ::IncrementCountWorker.perform_async(params[:post_id])
+      redirect_to root_path
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
 
   root to: "home#index"
 
+  post "/increment_async", to: "home#increment_async"
+
 end
